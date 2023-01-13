8e55f9055e2ca33194854ba9af02f39726c311d9
enipx <progenipx@gmail.com>
2023-01-13 18:33:55 +0100

    feat: remove website

--- web/components/FluidContainer.js
+++
@@ -1,9 +0,0 @@
-const FluidContainer = ({ children, style, css }) => {
-  return (
-    <div className={`fluid ${style || ''}`} style={css}>
-      {children}
-    </div>
-  )
-}
-
-export default FluidContainer
--- web/utils/variable.js
+++
@@ -1,9 +0,0 @@
-export const getSassVariable = (opt) => {
-  return fetch('/csx-configs.json')
-    .then(res => {
-      if (res.ok) {
-        return res.json();
-      }
-      throw new Error('[Error]: Loading css variables');
-    })
-}
